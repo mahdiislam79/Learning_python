0da763a9128073d4b1330c317ceebff9be9865ea
Mahdi Islam <mahdiiut079@gmail.com>
2022-11-11 00:12:04 +0600

    creating a function to check who wins

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -6,5 +6,20 @@ def get_choices():
     computer_choice = random.choice(options)
     choices = {'player': player_choice,'computer': computer_choice}
     return choices
+
+def check_win(player, computer):
+    print(f"You chose {player}, computer chose {computer}")
+    if player==computer:
+        return "It's a tie"
+    elif player == 'rock':
+        if computer == 'scissors':
+            return "Player wins"
+        else: return 'Computer wins'
+    elif player == 'paper':
+            if computer == 'scissors':
+                return "Computer wins"
+            else: return 'Player wins'
+
 choices = get_choices()
-print(choices)
+result = check_win(choices['player'],choices['computer'])
+print(result)
