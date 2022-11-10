f9bc299f2ea5cb1618f056306921c58da1d699e9
Mahdi Islam <mahdiiut079@gmail.com>
2022-11-10 22:30:50 +0600

    Adding code for closure

---
+++ Closure.py
@@ -0,0 +1,15 @@
+#Closure
+def counter():
+  count = 0
+
+  def increment():
+    nonlocal count
+    count+=1
+    return count
+
+  return increment
+  
+increment = counter()
+print(increment())
+print(increment())
+print(increment())
