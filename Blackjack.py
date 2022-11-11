ac9b2a0af01d9137479ac597a069d3a53a00049d
Mahdi Islam <mahdiiut079@gmail.com>
2022-11-11 21:00:30 +0600

    Creating Card and Deck classes

--- Blackjack.py
+++ Blackjack.py
@@ -1,36 +1,46 @@
 import random
-cards = []
-suits = ['spades', 'clubs', 'diamonds', 'hearts']
-ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-for suit in suits:
-    for rank in ranks:
-        cards.append([suit, rank])
 
+class Card:
+    def __init__(self,suit,rank):
+        self.suit = suit
+        self.rank = rank
+    def __str__(self):
+        return f"{self.rank['rank']} of {self.suit}"
 
-def shuffle():
-    random.shuffle(cards)
+class Deck:
+    def __init__(self):
+        
+        self.cards = []
+        suits = ['spades', 'clubs', 'diamonds', 'hearts']
+        ranks = [
+            {'rank':'A', 'value':'11'},
+            {'rank':'2', 'value':'2'},
+            {'rank':'3', 'value':'3'},
+            {'rank':'4', 'value':'4'},
+            {'rank':'5', 'value':'5'},
+            {'rank':'6', 'value':'6'},
+            {'rank':'7', 'value':'7'},
+            {'rank':'8', 'value':'8'},
+            {'rank':'9', 'value':'9'},
+            {'rank':'10', 'value':'10'},
+            {'rank':'K', 'value':'10'},
+            {'rank':'Q', 'value':'10'},
+            {'rank':'J', 'value':'10'},
+            ]
+        for suit in suits:
+            for rank in ranks:
+                self.cards.append(Card(suit,rank))
+                
+    def shuffle(self):
+        if self.cards > 1:
+            random.shuffle(self.cards)
+            
+    def deal(self,number):
+        cards_dealt = []
+        for n in range(number):
+            if len(self.cards) > 0:
+                cards_dealt.append(self.cards.pop())
+        return cards_dealt
 
-
-def deal(number):
-    cards_dealt = []
-    for n in range(number):
-        card = cards.pop()
-        cards_dealt.append(card)
-    return cards_dealt
-
-
-shuffle()
-cards_dealt = deal(2)
-card = cards_dealt[0]
-
-rank = card[1]
-
-if rank == 'A':
-    value = 11
-elif rank == 'K' or rank == 'J' or rank == 'Q':
-    value = 10
-else:
-    value = rank
-
-rank_dict = {'rank': rank, 'value': value}
-print(rank_dict['rank'], rank_dict['value'])
+card1 = Card('hearts',{'rank':'A','value':'11'})
+print(card1)
