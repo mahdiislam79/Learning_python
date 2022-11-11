4f21629f56221ab59765443681c8853626dda338
Mahdi Islam <mahdiiut079@gmail.com>
2022-11-11 18:30:41 +0600

    modifiying some functions

--- Blackjack.py
+++ Blackjack.py
@@ -5,11 +5,32 @@ ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 for suit in suits:
     for rank in ranks:
         cards.append([suit, rank])
+
+
 def shuffle():
     random.shuffle(cards)
-def deal():
-    card = cards.pop()
-    return card
+
+
+def deal(number):
+    cards_dealt = []
+    for n in range(number):
+        card = cards.pop()
+        cards_dealt.append(card)
+    return cards_dealt
+
+
 shuffle()
-card = deal()
-print(card)
+cards_dealt = deal(2)
+card = cards_dealt[0]
+
+rank = card[1]
+
+if rank == 'A':
+    value = 11
+elif rank == 'K' or rank == 'J' or rank == 'Q':
+    value = 10
+else:
+    value = rank
+
+rank_dict = {'rank': rank, 'value': value}
+print(rank_dict['rank'], rank_dict['value'])
