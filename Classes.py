c41021c1508f29f0cc0d753d8c13916892f43073
Mahdi Islam <mahdiiut079@gmail.com>
2022-11-11 12:44:51 +0600

    Adding docstrings

--- Classes.py
+++ Classes.py
@@ -1,14 +1,16 @@
 #Classes
 class Animal:
+    """ A class representing an Animal """
     def walk(self):
         print('Walking......')
 class Dog(Animal): #inheritance: where the Dog class inherited the walk function from the Animal class.
-
-    
+    """ A class representing a dog where it inherits the characteristics of Animal function"""
     def __init__(self,name,age): # Constructor
+        """ Initialize a new dog"""    
         self.name = name
         self.age =  age
     def bark(self): # method 
+        """Let the dog bark"""
         print('woof')
 
 roger = Dog("Roger", 8)
@@ -19,3 +21,5 @@ roger.bark()
 roger.walk()
 def dog():
     print('woof!')
+    
+print(help(Dog))
